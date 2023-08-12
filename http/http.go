0c818fa59b53b6314bff22137fb79450812690c4
Zaba505 <cakub6@gmx.com>
2023-08-11 23:18:44 -0400

    feat(http): rough out runtime

--- http/http.go
+++ http/http.go
@@ -4,3 +4,46 @@
 // https://opensource.org/licenses/MIT
 
 package http
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type runtimeOptions struct{}
+
+type RuntimeOption func(*runtimeOptions)
+
+type Runtime struct {
+	h http.Handler
+}
+
+func NewRuntime(h http.Handler, opts ...RuntimeOption) *Runtime {
+	return &Runtime{
+		h: h,
+	}
+}
+
+func (rt *Runtime) Run(ctx context.Context) error {
+	ls, err := net.Listen("http", ":8080")
+	if err != nil {
+		return err
+	}
+
+	srv := &http.Server{
+		Handler: rt.h,
+	}
+
+	g, gctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		<-gctx.Done()
+		return srv.Shutdown(context.Background())
+	})
+	g.Go(func() error {
+		return srv.Serve(ls)
+	})
+	return g.Wait()
+}
