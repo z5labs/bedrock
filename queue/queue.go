f139e043540e900e5393b4ac260b53a3922570f3
Zaba505 <cakub6@gmx.com>
2023-12-10 17:36:39 -0500

    docs(issue-3): add package comment

--- queue/queue.go
+++ queue/queue.go
@@ -3,6 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// Package queue provides multiple patterns which implements the app.Runtime interface.
 package queue
 
 import (
