bb2ab98f7cdf299a805b32623796cc50247d7abb
Richard Carson Derr <cakub6@gmx.com>
2023-12-15 23:12:23 -0500

    story(slogfield): add plurals (#36)
    feat(issue-24): implement plural log field helpers for simple types

--- pkg/slogfield/slogfield.go
+++ pkg/slogfield/slogfield.go
@@ -21,6 +21,11 @@ func Bool(key string, value bool) slog.Attr {
 	return slog.Bool(key, value)
 }
 
+// Bools
+func Bools(key string, values []bool) slog.Attr {
+	return slog.Any(key, values)
+}
+
 // Duration
 func Duration(key string, d time.Duration) slog.Attr {
 	return slog.Duration(key, d)
@@ -36,9 +41,19 @@ func String(key, value string) slog.Attr {
 	return slog.String(key, value)
 }
 
+// Strings
+func Strings(key string, values []string) slog.Attr {
+	return slog.Any(key, values)
+}
+
 // Int
 func Int(key string, n int) slog.Attr {
 	return slog.Int(key, n)
+}
+
+// Ints
+func Ints(key string, ns []int) slog.Attr {
+	return slog.Any(key, ns)
 }
 
 // Int8
@@ -46,9 +61,19 @@ func Int8(key string, n int8) slog.Attr {
 	return slog.Int64(key, int64(n))
 }
 
-// Int8
+// Int8s
+func Int8s(key string, ns []int8) slog.Attr {
+	return slog.Any(key, ns)
+}
+
+// Int16
 func Int16(key string, n int16) slog.Attr {
 	return slog.Int64(key, int64(n))
+}
+
+// Int16s
+func Int16s(key string, ns []int16) slog.Attr {
+	return slog.Any(key, ns)
 }
 
 // Int32
@@ -56,9 +81,19 @@ func Int32(key string, n int32) slog.Attr {
 	return slog.Int64(key, int64(n))
 }
 
+// Int32s
+func Int32s(key string, ns []int32) slog.Attr {
+	return slog.Any(key, ns)
+}
+
 // Int64
 func Int64(key string, n int64) slog.Attr {
 	return slog.Int64(key, n)
+}
+
+// Int64s
+func Int64s(key string, ns []int64) slog.Attr {
+	return slog.Any(key, ns)
 }
 
 // Uint
@@ -66,9 +101,19 @@ func Uint(key string, n uint) slog.Attr {
 	return slog.Uint64(key, uint64(n))
 }
 
+// Uints
+func Uints(key string, ns []uint) slog.Attr {
+	return slog.Any(key, ns)
+}
+
 // Uint8
 func Uint8(key string, n uint8) slog.Attr {
 	return slog.Uint64(key, uint64(n))
+}
+
+// Uint8s
+func Uint8s(key string, ns []uint8) slog.Attr {
+	return slog.Any(key, ns)
 }
 
 // Uint16
@@ -76,12 +121,27 @@ func Uint16(key string, n uint16) slog.Attr {
 	return slog.Uint64(key, uint64(n))
 }
 
+// Uint16s
+func Uint16s(key string, ns []uint16) slog.Attr {
+	return slog.Any(key, ns)
+}
+
 // Uint32
 func Uint32(key string, n uint32) slog.Attr {
 	return slog.Uint64(key, uint64(n))
 }
 
+// Uint32s
+func Uint32s(key string, ns []uint32) slog.Attr {
+	return slog.Any(key, ns)
+}
+
 // Uint64
 func Uint64(key string, n uint64) slog.Attr {
 	return slog.Uint64(key, n)
+}
+
+// Uint64s
+func Uint64s(key string, ns []uint64) slog.Attr {
+	return slog.Any(key, ns)
 }
