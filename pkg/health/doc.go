ee88006ab897da2b0679afecf85de8be98231259
Richard Carson Derr <cakub6@gmx.com>
2024-01-19 22:27:07 -0500

    doc(issue-48): document types, methods, and package

--- pkg/health/doc.go
+++ pkg/health/doc.go
@@ -3,5 +3,5 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-// Package health
+// Package health defines K8s inspired health metrics.
 package health
