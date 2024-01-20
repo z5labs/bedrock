81fb65bfaec6ebccb7de964b85d1cf41c72f03c7
Richard Carson Derr <cakub6@gmx.com>
2024-01-20 15:42:25 -0500

    doc(issue-48): documented function and types

--- pkg/config/doc.go
+++ pkg/config/doc.go
@@ -3,5 +3,5 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-// Package config provides an useful wrapper for the popular spf13/viper package.
+// Package config provides a useful wrapper for the popular spf13/viper package.
 package config
