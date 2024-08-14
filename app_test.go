95349546738b18989569fe39b89df0525a188eab
Richard Carson Derr <cakub6@gmx.com>
2024-08-13 23:55:23 -0400

    test(issue-233): fully tested new bedrock package api

--- app_test.go
+++ app_test.go
@@ -8,6 +8,7 @@ package bedrock
 import (
 	"context"
 	"errors"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,6 +19,14 @@ type configSourceFunc func(config.Store) error
 
 func (f configSourceFunc) Apply(store config.Store) error {
 	return f(store)
+}
+
+var unmarshalErr = errors.New("failed to unmarshal")
+
+type UnmarshalTextFailure struct{}
+
+func (x UnmarshalTextFailure) UnmarshalText(b []byte) error {
+	return unmarshalErr
 }
 
 func TestRun(t *testing.T) {
@@ -43,6 +52,79 @@ func TestRun(t *testing.T) {
 				return
 			}
 			if !assert.ErrorIs(t, ierr, srcErr) {
+				return
+			}
+		})
+
+		t.Run("if the config.Manager fails to unmarshal the custom config", func(t *testing.T) {
+			type myConfig struct {
+				Value UnmarshalTextFailure `config:"value"`
+			}
+			b := AppBuilderFunc[myConfig](func(ctx context.Context, cfg myConfig) (App, error) {
+				return nil, nil
+			})
+
+			err := Run(context.Background(), b, config.FromYaml(strings.NewReader(`value: hello`)))
+
+			var ierr ConfigUnmarshalError
+			if !assert.ErrorAs(t, err, &ierr) {
+				return
+			}
+			if !assert.NotEmpty(t, ierr.Error()) {
+				return
+			}
+			if !assert.ErrorIs(t, ierr, unmarshalErr) {
+				return
+			}
+		})
+
+		t.Run("if the AppBuilder fails to build the App", func(t *testing.T) {
+			type myConfig struct {
+				Value string `config:"value"`
+			}
+
+			buildErr := errors.New("failed to build")
+			b := AppBuilderFunc[myConfig](func(ctx context.Context, cfg myConfig) (App, error) {
+				return nil, buildErr
+			})
+
+			err := Run(context.Background(), b, config.FromYaml(strings.NewReader(`value: hello`)))
+
+			var ierr AppBuildError
+			if !assert.ErrorAs(t, err, &ierr) {
+				return
+			}
+			if !assert.NotEmpty(t, ierr.Error()) {
+				return
+			}
+			if !assert.ErrorIs(t, ierr, buildErr) {
+				return
+			}
+		})
+
+		t.Run("if the App fails to run", func(t *testing.T) {
+			type myConfig struct {
+				Value string `config:"value"`
+			}
+
+			runErr := errors.New("failed to build")
+			b := AppBuilderFunc[myConfig](func(ctx context.Context, cfg myConfig) (App, error) {
+				app := appFunc(func(ctx context.Context) error {
+					return runErr
+				})
+				return app, nil
+			})
+
+			err := Run(context.Background(), b, config.FromYaml(strings.NewReader(`value: hello`)))
+
+			var ierr AppRunError
+			if !assert.ErrorAs(t, err, &ierr) {
+				return
+			}
+			if !assert.NotEmpty(t, ierr.Error()) {
+				return
+			}
+			if !assert.ErrorIs(t, ierr, runErr) {
 				return
 			}
 		})
