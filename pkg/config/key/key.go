7bdbdde616a7a9948f24d5b6ab6e5e31bafd4450
Richard Carson Derr <cakub6@gmx.com>
2024-08-13 22:34:09 -0400

    doc(issue-221): added docs for key package

--- pkg/config/key/key.go
+++ pkg/config/key/key.go
@@ -3,18 +3,22 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// Package key provides types for strongly typed keys in key value pairs.
 package key
 
 import (
 	"strings"
 )
 
+// Keyer is a common interface all value key types must implement.
 type Keyer interface {
 	Key() string
 }
 
+// Chain represents nested keys.
 type Chain []Keyer
 
+// Key implements the [Keyer] interface.
 func (k Chain) Key() string {
 	ss := make([]string, len(k))
 	for i := range len(k) {
@@ -23,8 +27,10 @@ func (k Chain) Key() string {
 	return strings.Join(ss, ".")
 }
 
+// Name represents a single key. Name can be used other keys.
 type Name string
 
+// Key implements the [Keyer] interface.
 func (k Name) Key() string {
 	return string(k)
 }
