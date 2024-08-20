0ba917b9edcc544a15a98c5625bd48bb866953ec
Richard Carson Derr <cakub6@gmx.com>
2024-08-19 23:30:57 -0400

    story(issue-242): rest inject path params for use in endpoint handler (#245)
    * feat(issue-242): implemented injector for path values and corresponding getter
    
    * feat(issue-242): added path param injectors and openapi specs
    
    * chore(docs): updated coverage badge.
    
    * test(issue-242): added cases for path, header, and query param value injection
    
    * chore(docs): updated coverage badge.
    
    * feat(issue-242): register path params with openapi spec
    
    * refactor(issue-242): simplify structure to make it more readable
    
    * test(issue-242): added coverage to the extra constructor funcs
    
    * chore(docs): updated coverage badge.
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- rest/endpoint/inject.go
+++ rest/endpoint/inject.go
@@ -8,6 +8,7 @@ package endpoint
 import (
 	"context"
 	"net/http"
+	"net/url"
 )
 
 type injector func(context.Context, http.ResponseWriter, *http.Request) context.Context
@@ -27,9 +28,48 @@ var (
 	injectResponseHeadersKey = injectKey("injectResponseHeadersKey")
 )
 
+type injectPathParamKey string
+
+// PathValue returns the value for a path parameter of the given name.
+// The empty string will be returned either if the path param is not set
+// or not found.
+func PathValue(ctx context.Context, name string) string {
+	s, _ := ctx.Value(injectPathParamKey(name)).(string)
+	return s
+}
+
+func injectPathParam(name string) injector {
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) context.Context {
+		ctx = context.WithValue(ctx, injectPathParamKey(name), r.PathValue(name))
+		return ctx
+	}
+}
+
+// QueryValue returns the value for a query parameter of the given name.
+// The empty string will be returned either if the query param is not set
+// or not found.
+func QueryValue(ctx context.Context, name string) string {
+	vals, ok := ctx.Value(injectQueryParamsKey).(url.Values)
+	if !ok {
+		return ""
+	}
+	return vals.Get(name)
+}
+
 func injectQueryParams(ctx context.Context, w http.ResponseWriter, r *http.Request) context.Context {
 	ctx = context.WithValue(ctx, injectQueryParamsKey, r.URL.Query())
 	return ctx
+}
+
+// HeaderValue returns the value for a header of the given name.
+// The empty string will be returned either if the header is not set
+// or not found.
+func HeaderValue(ctx context.Context, name string) string {
+	headers, ok := ctx.Value(injectHeadersKey).(http.Header)
+	if !ok {
+		return ""
+	}
+	return headers.Get(name)
 }
 
 func injectHeaders(ctx context.Context, w http.ResponseWriter, r *http.Request) context.Context {
@@ -40,7 +80,7 @@ func injectHeaders(ctx context.Context, w http.ResponseWriter, r *http.Request) 
 // SetResponseHeader allows you set a custom response header.
 func SetResponseHeader(ctx context.Context, key, value string) {
 	headers, ok := ctx.Value(injectResponseHeadersKey).(http.Header)
-	if !ok && headers != nil {
+	if !ok || headers == nil {
 		return
 	}
 
