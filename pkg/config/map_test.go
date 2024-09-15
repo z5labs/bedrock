e19b559cfbc58246489151b9ef8e934efb8ef20c
Richard Carson Derr <cakub6@gmx.com>
2024-09-14 23:42:58 -0400

    story(issue-269): config implement source interface on manager (#270)
    * refactor(issue-269): just use map type as store and source
    
    * feat(issue-269): implement config source interface on config manager
    
    * chore(docs): updated coverage badge.
    
    * test(issue-269): add some cases around read optimizations
    
    * chore(docs): updated coverage badge.
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- pkg/config/map_test.go
+++ pkg/config/map_test.go
@@ -153,3 +153,57 @@ func TestMap_Apply(t *testing.T) {
 		})
 	})
 }
+
+type myKeyer string
+
+func (myKeyer) Key() string {
+	return "my key"
+}
+
+func TestMap_Set(t *testing.T) {
+	t.Run("will return an error", func(t *testing.T) {
+		t.Run("if an known key.Keyer is used", func(t *testing.T) {
+			store := make(Map)
+			err := store.Set(myKeyer("hello"), "world")
+
+			var ierr UnknownKeyerError
+			if !assert.ErrorAs(t, err, &ierr) {
+				return
+			}
+			if !assert.NotEmpty(t, ierr.Error()) {
+				return
+			}
+		})
+
+		t.Run("if an empty key.Chain is used", func(t *testing.T) {
+			store := make(Map)
+			err := store.Set(key.Chain{}, "world")
+
+			var ierr EmptyKeyChainError
+			if !assert.ErrorAs(t, err, &ierr) {
+				return
+			}
+			if !assert.NotEmpty(t, ierr.Error()) {
+				return
+			}
+		})
+
+		t.Run("if the value type is attempted to be changed while overriding an existing key", func(t *testing.T) {
+			store := make(Map)
+			err := store.Set(key.Name("hello"), "world")
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			err = store.Set(key.Chain{key.Name("hello"), key.Name("bob")}, "world")
+
+			var ierr UnexpectedKeyValueTypeError
+			if !assert.ErrorAs(t, err, &ierr) {
+				return
+			}
+			if !assert.NotEmpty(t, ierr.Error()) {
+				return
+			}
+		})
+	})
+}
