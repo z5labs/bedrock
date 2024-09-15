efbbfbfade995b593a9938833be93fa2c5326007
Richard Carson Derr <cakub6@gmx.com>
2024-09-14 21:26:23 -0400

    story(issue-267): appbuilder implement a middleware package for app builders (#268)
    * feat(issue-267): initialize appbuilder package
    
    * feat(issue-267): implement otel appbuilder middleware
    
    * chore(docs): updated coverage badge.
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ pkg/appbuilder/doc.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 Z5Labs and Contributors
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// Package appbuilder provides helpers for common bedrock.AppBuilder implementation patterns.
+package appbuilder
