74d5fa67dad062bf50ad1ba9e14d411456b56709
Richard Carson Derr <cakub6@gmx.com>
2024-09-15 16:10:34 -0400

    story(issue-273): app do not ignore lifecycle hook errors in post run (#276)
    * feat(issue-273): capture post run hook error
    
    * test(issue-273): add examples for recover and lifecycle hooks
    
    * test(issue-273): added examples for otel middleware

---
+++ pkg/app/app_example_test.go
@@ -0,0 +1,104 @@
+// Copyright (c) 2024 Z5Labs and Contributors
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package app
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/z5labs/bedrock"
+)
+
+func ExampleRecover() {
+	app := runFunc(func(ctx context.Context) error {
+		panic("hello world")
+		return nil
+	})
+
+	err := Recover(app).Run(context.Background())
+
+	var perr PanicError
+	if !errors.As(err, &perr) {
+		fmt.Println("should be a panic error since \"hello world\" does not implement error interface.")
+		return
+	}
+
+	fmt.Println(perr.Value)
+	// Output: hello world
+}
+
+func ExampleRecover_errorValue() {
+	app := runFunc(func(ctx context.Context) error {
+		panic(errors.New("hello world"))
+		return nil
+	})
+
+	err := Recover(app).Run(context.Background())
+	if errors.Is(err, PanicError{}) {
+		fmt.Println("should not be a panic error since errors.New() does implement error interface.")
+		return
+	}
+
+	fmt.Println(err)
+	// Output: hello world
+}
+
+func ExampleWithLifecycleHooks() {
+	var app bedrock.App = runFunc(func(ctx context.Context) error {
+		return nil
+	})
+
+	postRun := LifecycleHookFunc(func(ctx context.Context) error {
+		fmt.Println("ran post run hook")
+		return nil
+	})
+
+	app = WithLifecycleHooks(app, Lifecycle{
+		PostRun: postRun,
+	})
+
+	err := app.Run(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Output: ran post run hook
+}
+
+func ExampleWithLifecycleHooks_unrecoveredPanic() {
+	var app bedrock.App = runFunc(func(ctx context.Context) error {
+		panic("hello world")
+		return nil
+	})
+
+	postRun := LifecycleHookFunc(func(ctx context.Context) error {
+		fmt.Println("ran post run hook")
+		return nil
+	})
+
+	app = WithLifecycleHooks(app, Lifecycle{
+		PostRun: postRun,
+	})
+
+	run := func(ctx context.Context) error {
+		// recover here so the panic doesn't crash the example
+		defer func() {
+			recover()
+		}()
+
+		return app.Run(ctx)
+	}
+
+	err := run(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Output: ran post run hook
+}
