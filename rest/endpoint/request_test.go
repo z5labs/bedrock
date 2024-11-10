f741ef35cd376ea7a89734e54cb293d187ff8cf5
Richard Carson Derr <cakub6@gmx.com>
2024-11-09 22:26:34 -0500

    test(issue-324): added case for consumes yaml

--- rest/endpoint/request_test.go
+++ rest/endpoint/request_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 type successValidator struct{}
@@ -133,6 +134,131 @@ func TestConsumesJson(t *testing.T) {
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(`{}`))
 			r.Header.Set("Content-Type", JsonRequest[noop]{}.ContentType())
+
+			op.ServeHTTP(w, r)
+
+			resp := w.Result()
+			if !assert.Equal(t, DefaultStatusCode, resp.StatusCode) {
+				return
+			}
+			if !assert.Nil(t, caughtError) {
+				return
+			}
+		})
+	})
+}
+
+func TestConsumesYaml(t *testing.T) {
+	t.Run("will return an error while reading", func(t *testing.T) {
+		t.Run("if the request body is not valid yaml", func(t *testing.T) {
+			h := noopHandler[Empty, Empty]{}
+
+			var caughtError error
+			op := NewOperation(
+				ConsumesYaml(h),
+				OnError(errorHandlerFunc(func(ctx context.Context, w http.ResponseWriter, err error) {
+					caughtError = err
+
+					w.WriteHeader(DefaultErrorStatusCode)
+				})),
+			)
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(`hello world`))
+			r.Header.Set("Content-Type", YamlRequest[Empty]{}.ContentType())
+
+			op.ServeHTTP(w, r)
+
+			resp := w.Result()
+			if !assert.Equal(t, DefaultErrorStatusCode, resp.StatusCode) {
+				return
+			}
+
+			var terr *yaml.TypeError
+			if !assert.ErrorAs(t, caughtError, &terr) {
+				return
+			}
+		})
+	})
+
+	t.Run("will return a validation error", func(t *testing.T) {
+		t.Run("if the inner type fails to validate", func(t *testing.T) {
+			h := noopHandler[InvalidRequest, Empty]{}
+
+			var caughtError error
+			op := NewOperation(
+				ConsumesYaml(h),
+				OnError(errorHandlerFunc(func(ctx context.Context, w http.ResponseWriter, err error) {
+					caughtError = err
+
+					w.WriteHeader(DefaultErrorStatusCode)
+				})),
+			)
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(`{}`))
+			r.Header.Set("Content-Type", YamlRequest[InvalidRequest]{}.ContentType())
+
+			op.ServeHTTP(w, r)
+
+			resp := w.Result()
+			if !assert.Equal(t, DefaultErrorStatusCode, resp.StatusCode) {
+				return
+			}
+
+			if !assert.Equal(t, errInvalidRequest, caughtError) {
+				return
+			}
+		})
+	})
+
+	t.Run("will not return a validation error", func(t *testing.T) {
+		t.Run("if the inner types successfully validates", func(t *testing.T) {
+			h := noopHandler[successValidator, Empty]{}
+
+			var caughtError error
+			op := NewOperation(
+				ConsumesYaml(h),
+				OnError(errorHandlerFunc(func(ctx context.Context, w http.ResponseWriter, err error) {
+					caughtError = err
+
+					w.WriteHeader(DefaultErrorStatusCode)
+				})),
+			)
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(`{}`))
+			r.Header.Set("Content-Type", YamlRequest[successValidator]{}.ContentType())
+
+			op.ServeHTTP(w, r)
+
+			resp := w.Result()
+			if !assert.Equal(t, DefaultStatusCode, resp.StatusCode) {
+				return
+			}
+			if !assert.Nil(t, caughtError) {
+				return
+			}
+		})
+
+		t.Run("if the inner type does not implement the Validator interface", func(t *testing.T) {
+			type noop struct{}
+
+			h := noopHandler[noop, Empty]{}
+
+			var caughtError error
+			op := NewOperation(
+				ConsumesYaml(h),
+				OnError(errorHandlerFunc(func(ctx context.Context, w http.ResponseWriter, err error) {
+					caughtError = err
+
+					w.WriteHeader(DefaultErrorStatusCode)
+				})),
+			)
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(`{}`))
+			r.Header.Set("Content-Type", YamlRequest[noop]{}.ContentType())
 
 			op.ServeHTTP(w, r)
 
