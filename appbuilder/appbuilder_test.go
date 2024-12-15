e44e7cbef0eda5bba34aff47cabc1baf935003b4
Richard Carson Derr <cakub6@gmx.com>
2024-12-15 01:15:20 -0500

    refactor(issue-342): simplify otel app builder middleware

--- appbuilder/appbuilder_test.go
+++ appbuilder/appbuilder_test.go
@@ -19,11 +19,11 @@ func TestRecover(t *testing.T) {
 	t.Run("will return an error", func(t *testing.T) {
 		t.Run("if the underlying App returns an error", func(t *testing.T) {
 			buildErr := errors.New("failed to build")
-			builder := Recover(bedrock.AppBuilderFunc[config](func(ctx context.Context, cfg config) (bedrock.App, error) {
+			builder := Recover(bedrock.AppBuilderFunc[struct{}](func(ctx context.Context, cfg struct{}) (bedrock.App, error) {
 				return nil, buildErr
 			}))
 
-			_, err := builder.Build(context.Background(), config{})
+			_, err := builder.Build(context.Background(), struct{}{})
 			if !assert.Equal(t, buildErr, err) {
 				return
 			}
@@ -31,24 +31,24 @@ func TestRecover(t *testing.T) {
 
 		t.Run("if the underlying App panics with an error value", func(t *testing.T) {
 			buildErr := errors.New("failed to build")
-			builder := Recover(bedrock.AppBuilderFunc[config](func(ctx context.Context, cfg config) (bedrock.App, error) {
+			builder := Recover(bedrock.AppBuilderFunc[struct{}](func(ctx context.Context, cfg struct{}) (bedrock.App, error) {
 				panic(buildErr)
 				return nil, nil
 			}))
 
-			_, err := builder.Build(context.Background(), config{})
+			_, err := builder.Build(context.Background(), struct{}{})
 			if !assert.ErrorIs(t, err, buildErr) {
 				return
 			}
 		})
 
 		t.Run("if the underlying App panics with a non-error value", func(t *testing.T) {
-			builder := Recover(bedrock.AppBuilderFunc[config](func(ctx context.Context, cfg config) (bedrock.App, error) {
+			builder := Recover(bedrock.AppBuilderFunc[struct{}](func(ctx context.Context, cfg struct{}) (bedrock.App, error) {
 				panic("hello world")
 				return nil, nil
 			}))
 
-			_, err := builder.Build(context.Background(), config{})
+			_, err := builder.Build(context.Background(), struct{}{})
 
 			var perr bedrock.PanicError
 			if !assert.ErrorAs(t, err, &perr) {
