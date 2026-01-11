29e7e26515cb1a55977cfb9fc2666be205ee4605
Richard Carson Derr <cakub6@gmx.com>
2026-01-11 09:46:29 -0500

    refactor(config): functional values are now the standard

--- config/config_example_test.go
+++ config/config_example_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Z5Labs and Contributors
+// Copyright (c) 2026 Z5Labs and Contributors
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
@@ -6,254 +6,26 @@
 package config
 
 import (
+	"bytes"
+	"context"
+	"encoding/binary"
 	"fmt"
-	"os"
-	"strings"
 )
 
-func ExampleRead() {
-	src := Map{
-		"hello": "world",
-	}
-
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"hello"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	// Output: world
-}
-
-func ExampleRead_map() {
-	src := Map{
-		"hello": "world",
-		"xs":    []int{1, 2, 3},
-		"a": map[string]any{
-			"b": 1.2,
-		},
-	}
-
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"hello"`
-		Xs    []int  `config:"xs"`
-		A     struct {
-			B float64 `config:"b"`
-		} `config:"a"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	fmt.Println(cfg.Xs)
-	fmt.Println(cfg.A.B)
-	// Output: world
-	// [1 2 3]
-	// 1.2
-}
-
-func ExampleRead_yaml() {
-	r := strings.NewReader(`
-hello: world
-xs:
-  - 1
-  - 2
-  - 3
-a:
-  b: 1.2
-`)
-	src := FromYaml(r)
-
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"hello"`
-		Xs    []int  `config:"xs"`
-		A     struct {
-			B float64 `config:"b"`
-		} `config:"a"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	fmt.Println(cfg.Xs)
-	fmt.Println(cfg.A.B)
-	// Output: world
-	// [1 2 3]
-	// 1.2
-}
-
-func ExampleRead_json() {
-	r := strings.NewReader(`{
-		"hello": "world",
-		"xs":[
-		  1,
-		  2,
-		  3
-		],
-		"a":{
-		  "b": 1.2
-		}
-	}`)
-	src := FromJson(r)
-
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"hello"`
-		Xs    []int  `config:"xs"`
-		A     struct {
-			B float64 `config:"b"`
-		} `config:"a"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	fmt.Println(cfg.Xs)
-	fmt.Println(cfg.A.B)
-	// Output: world
-	// [1 2 3]
-	// 1.2
-}
-
-func ExampleRead_env() {
-	os.Setenv("HELLO", "world")
-	defer os.Unsetenv("HELLO")
-
-	src := FromEnv()
-
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"HELLO"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	// Output: world
-}
-
-func ExampleRead_textTemplateRenderer() {
-	r := strings.NewReader(`hello: {{ myName }}`)
-	ttr := RenderTextTemplate(
-		r,
-		TemplateFunc("myName", func() string {
-			return "bob"
-		}),
+func Example() {
+	myIntFromString, _ := Read(
+		context.Background(),
+		Default(10, Int64FromString(Env("MY_INT"))),
 	)
 
-	src := FromYaml(ttr)
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"hello"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	// Output: bob
-}
-
-func ExampleRead_textTemplateRenderer_customDelims() {
-	r := strings.NewReader(`hello: (( myName ))`)
-	ttr := RenderTextTemplate(
-		r,
-		TemplateDelims("((", "))"),
-		TemplateFunc("myName", func() string {
-			return "bob"
-		}),
+	myIntFromBytes, _ := Read(
+		context.Background(),
+		Default(10, Int64FromBytes(binary.LittleEndian, ReaderOf(bytes.NewReader([]byte{1, 1, 1, 1, 1, 1, 1, 1})))),
 	)
 
-	src := FromYaml(ttr)
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"hello"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	// Output: bob
-}
-
-func ExampleRead_fileReader() {
-	r := NewFileReader(os.DirFS("."), "testdata/config.yaml")
-
-	src := FromYaml(r)
-	m, err := Read(src)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cfg struct {
-		Hello string `config:"hello"`
-	}
-	err = m.Unmarshal(&cfg)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(cfg.Hello)
-	// Output: world
+	fmt.Println(myIntFromString)
+	fmt.Println(myIntFromBytes)
+	// Output:
+	// 10
+	// 72340172838076673
 }
